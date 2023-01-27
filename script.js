6ded35a47be6dd5e054dced2969c40483ef2fa5b
Rohit Singh <r.singh789583@gmail.com>
2023-01-27 14:42:51 +0530

    i made my Portfolio website responsive

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Sofia"> -->
-/* font-family: 'Sofia', sans-serif; */
+var sidemu = document.getElementById("sidemenu");
+function openmenu(){
+	sidemu.style.right = "0"
+}
+function closemenu(){
+	sidemu.style.right = "-200px"
+	
+}
